dfb485e5bb6b2dac3b646aea6b533a05d0a506c7
Joe Carter <joe_e_carter@hotmail.co.uk>
2013-06-29 21:25:24 +0100

    Start of implementation of game state system

---
+++ RTS Game/RTS Game/GameState/BasicGameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RTS_Game
+{
+    //The base class for every game state
+    abstract class BasicGameState
+    {
+
+    }
+}
